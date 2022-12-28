1843ede486ce3af82ccea35c60be21ee7e0e545b
Roberto de Paiva Assumpção <betoassumpcao@protonmail.com>
2022-12-27 22:11:25 -0300

    feat: Verificando qual e quantas moedas

--- Módulo 1: C/Exercício 3: Dinheiro (versão fácil)/main.c
+++ Módulo 1: C/Exercício 3: Dinheiro (versão fácil)/main.c
@@ -8,15 +8,45 @@
 
 int main(int argc, char *argv[])
 {
-    float troco;
-    int troco_arredondado;
+    float dollar;
+    int centavos;
+    short qtd_moedas_usadas = 0, moedas_usadas[40], moeda[4] = {25,10,5,1}, i = 0;
+
+
+    // Pegando troco e convertendo em centavos.
     do
     {
         printf("Digite o troco devido:\n");
-        scanf("%f", &troco);
-    } while (troco <= 0.0);
+        scanf("%f", &dollar);
+    } while (dollar <= 0.0);
     
-    troco_arredondado = round(troco);
+    centavos = dollar * 100;
+    
+    // Verificando qual e quantas moedas usar para devolver ao cliente.
+    do
+    {
+        if(centavos - moeda[i] < 0)
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            centavos = centavos - moeda[i];
+            moedas_usadas[qtd_moedas_usadas] = moeda[i];
+            qtd_moedas_usadas++;
+        }
+
+    } while (centavos >= 1);
+    
+    printf("centavos restantes: %i\n", centavos);
+    printf("Foram usadas: %i\n", qtd_moedas_usadas);
+    
+    for (short j = 0; j < qtd_moedas_usadas; j++)
+    {
+        printf("As moedas usadas foram: %i\n", moedas_usadas[j]);
+    }
+    
 
     return 0;
 }
