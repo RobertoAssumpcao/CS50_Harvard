0806c9afe60f5510d25dad2b9a11c0d2563bdaa3
Roberto <betoassumpcao@protonmail.com>
2023-01-03 22:05:37 -0300

    feat: frase cifrada

--- Modulo2_array/Exercicio1_Caesar/main.c
+++ Modulo2_array/Exercicio1_Caesar/main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    char frase[501], frase_cifrada[501];
+    char frase[801], frase_cifrada[801];
     unsigned long n;
+    int flag;
 
-    if(argc == 2 && isdigit((int)*argv[1]))
+    if (argc == 2 && isdigit((int)* argv[1]))
     {
+        flag = atoi(argv[1]);
+
         printf("Digite a frase: ");
         scanf("%s", frase);
 
         n = strlen(frase);
         for (int i = 0; i < n; ++i)
         {
-            frase_cifrada[i] = frase[i] + *argv[1];
+            frase_cifrada[i] = (int)frase[i] + flag;
         }
         printf("Frase cifrada fica: %s\n", frase_cifrada);
     }
